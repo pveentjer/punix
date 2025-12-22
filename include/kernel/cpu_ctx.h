978e366d0500a0779674a45e44fe827006411f48
Peter Veentjer <alarmnummer@gmail.com>
2025-12-22 08:32:00 +0200

    Addr addr_base/addr_limit to cpu_ctx

--- include/kernel/cpu_ctx.h
+++ include/kernel/cpu_ctx.h
@@ -10,6 +10,10 @@ struct cpu_ctx
     // These only need to be set once and will never change again.
     uint16_t cs;
     uint16_t ds;
+
+    // User address-space description
+    uint32_t addr_base;
+    uint32_t addr_limit;
 };
 
 #endif //CPU_CTX_H
