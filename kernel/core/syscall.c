3eecb675d6ab4250ff249b123667d782249a597f
Peter Veentjer <alarmnummer@gmail.com>
2025-12-22 08:25:49 +0200

    Added skeleton uaddr relation to syscall.c

--- kernel/core/syscall.c
+++ kernel/core/syscall.c
@@ -1,3 +1,5 @@
+// kernel/sys_enter_dispatch.c (or wherever this lives)
+
 #include <stdint.h>
 #include "kernel/syscall.h"
 #include "kernel/console.h"
@@ -10,6 +12,16 @@
 #include "kernel/vfs.h"
 #include "kernel/mm.h"
 
+static inline void *uaddr(uint32_t user_ptr)
+{
+    return (void *)user_ptr;
+}
+
+static inline const void *uaddr_ro(uint32_t user_ptr)
+{
+    return (const void *)uaddr(user_ptr);
+}
+
 __attribute__((used))
 uint32_t sys_enter_dispatch_c(uint32_t nr,
                               uint32_t a1,
@@ -18,60 +30,68 @@ uint32_t sys_enter_dispatch_c(uint32_t nr,
                               uint32_t a4,
                               uint32_t a5)
 {
-    switch ((enum sys_call_nr) nr)
+    (void)a5;
+
+    switch ((enum sys_call_nr)nr)
     {
         case SYS_write:
         {
             sched_schedule();
-            return (uint32_t) vfs_write((int) a1, (const char *) a2, (size_t) a3);
+            return (uint32_t)vfs_write((int)a1, (const char *)uaddr_ro(a2), (size_t)a3);
         }
         case SYS_read:
         {
             sched_schedule();
-            return (uint32_t) vfs_read((int) a1, (void *) a2, (size_t) a3);
+            return (uint32_t)vfs_read((int)a1, uaddr(a2), (size_t)a3);
         }
         case SYS_open:
         {
-            return (uint32_t) vfs_open(&vfs, sched_current(), (const char *) a1, (int) a2, (int) a3);
+            return (uint32_t)vfs_open(&vfs, sched_current(),
+                                      (const char *)uaddr_ro(a1),
+                                      (int)a2,
+                                      (int)a3);
         }
         case SYS_close:
         {
-            return (uint32_t) vfs_close(&vfs, sched_current(), (int) a1);
+            return (uint32_t)vfs_close(&vfs, sched_current(), (int)a1);
         }
         case SYS_getdents:
         {
-            return (uint32_t) vfs_getdents((int) a1, (struct dirent *) a2, (unsigned int) a3);
+            return (uint32_t)vfs_getdents((int)a1, (struct dirent *)uaddr(a2), (unsigned int)a3);
         }
         case SYS_add_task:
         {
             sched_schedule();
-            return (uint32_t) sched_add_task((const char *) a1, (int) a2, (char **) a3, (char **) a4);
+            return (uint32_t)sched_add_task((const char *)uaddr_ro(a1),
+                                            (int)a2,
+                                            (char **)uaddr(a3),
+                                            (char **)uaddr(a4));
         }
         case SYS_fork:
         {
-            return (uint32_t) -ENOSYS;
+            return (uint32_t)-ENOSYS;
         }
         case SYS_execve:
         {
-            return (uint32_t) -ENOSYS;
+            return (uint32_t)-ENOSYS;
         }
         case SYS_exit:
         {
-            sched_exit((int) a1);
+            sched_exit((int)a1);
             __builtin_unreachable();
         }
         case SYS_kill:
         {
-            return (uint32_t) sched_kill((pid_t) a1, (int) a2);
+            return (uint32_t)sched_kill((pid_t)a1, (int)a2);
         }
         case SYS_waitpid:
         {
-            return (uint32_t) sched_waitpid((pid_t) a1, (int *) a2, (int) a3);
+            return (uint32_t)sched_waitpid((pid_t)a1, (int *)uaddr(a2), (int)a3);
         }
         case SYS_getpid:
         {
             sched_schedule();
-            return (uint32_t) sched_getpid();
+            return (uint32_t)sched_getpid();
         }
         case SYS_sched_yield:
         {
@@ -80,25 +100,25 @@ uint32_t sys_enter_dispatch_c(uint32_t nr,
         }
         case SYS_nice:
         {
-            (void) (int) a1;
-            return (uint32_t) 0;
+            (void)(int)a1;
+            return 0;
         }
         case SYS_brk:
         {
-            return (uint32_t) mm_brk((void *) a1);
+            return (uint32_t)mm_brk(uaddr(a1));
         }
         case SYS_chdir:
         {
             sched_schedule();
-            return (uint32_t) vfs_chdir((const char *) a1);
+            return (uint32_t)vfs_chdir((const char *)uaddr_ro(a1));
         }
         case SYS_getcwd:
         {
             sched_schedule();
-            return (uint32_t) vfs_getcwd((char *) a1, (size_t) a2);
+            return (uint32_t)vfs_getcwd((char *)uaddr(a1), (size_t)a2);
         }
 
         default:
-            return (uint32_t) -ENOSYS;
+            return (uint32_t)-ENOSYS;
     }
 }
