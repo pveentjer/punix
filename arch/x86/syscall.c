45770529dda0563611cd7d5b02720f9e1f74cfd6
Peter Veentjer <alarmnummer@gmail.com>
2025-12-24 09:55:47 +0200

    Removed selector logic from syscall

--- arch/x86/syscall.c
+++ arch/x86/syscall.c
@@ -1,26 +1,11 @@
 #include <stdint.h>
 #include "kernel/syscall.h"
-#include "include/gdt.h"   // GDT_KERNEL_DS
 
 __attribute__((naked, used))
 uint32_t sys_enter(void)
 {
     __asm__ volatile(
-            "pushw %%ds\n\t"
-            "pushw %%es\n\t"
-            "pushw %%fs\n\t"
-            "pushw %%gs\n\t"
-
-            /* load kernel data segments (preserve eax) */
-            "pushl %%eax\n\t"
-            "movw  %0, %%ax\n\t"
-            "movw  %%ax, %%ds\n\t"
-            "movw  %%ax, %%es\n\t"
-            "movw  %%ax, %%fs\n\t"
-            "movw  %%ax, %%gs\n\t"
-            "popl  %%eax\n\t"
-
-            /* regs -> cdecl args */
+        /* regs -> cdecl args */
             "pushl %%edi\n\t"
             "pushl %%esi\n\t"
             "pushl %%edx\n\t"
@@ -29,15 +14,9 @@ uint32_t sys_enter(void)
             "pushl %%eax\n\t"
             "call  sys_enter_dispatch_c\n\t"
             "addl  $24, %%esp\n\t"
-
-            /* restore segments */
-            "popw %%gs\n\t"
-            "popw %%fs\n\t"
-            "popw %%es\n\t"
-            "popw %%ds\n\t"
             "ret\n\t"
             :
-            : "i"((uint16_t)GDT_KERNEL_DS)
+            :
             : "memory"
             );
 }
