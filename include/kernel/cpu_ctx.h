cc4887bc93ab4dc039f91571a827d0d69f337f51
Peter Veentjer <alarmnummer@gmail.com>
2025-12-24 10:10:20 +0200

    Removed selector fields from cpu_ctx

--- include/kernel/cpu_ctx.h
+++ include/kernel/cpu_ctx.h
@@ -5,12 +5,6 @@ struct cpu_ctx
 {
     // we just need the sp/ss, all other cpu context is stored on the stack.
     uint32_t esp;
-
-
-    // These only need to be set once and will never change again.
-    uint16_t ss;
-    uint16_t cs;
-    uint16_t ds;
 };
 
 #endif //CPU_CTX_H
