e356909367309a74926214afc325d7fea7d727e6
Peter Veentjer <alarmnummer@gmail.com>
2025-12-25 15:22:03 +0200

    premain more logging

--- arch/x86/kpremain.asm
+++ arch/x86/kpremain.asm
@@ -37,226 +37,225 @@ extern __kernel_page_directory_va
 section .text.premain
 
 _kpremain:
-    cli
+     cli
 
-    mov ax, KERNEL_DS
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov esp, 589824
+     mov ax, KERNEL_DS
+     mov ds, ax
+     mov es, ax
+     mov fs, ax
+     mov gs, ax
+     mov ss, ax
+     mov esp, 589824
 
-    ; row 0: PT_PA
-    call get_pt_pa
-    mov ecx, 0
-    mov edx, 0
-    call vga_write_hex32
+     ; row 0
+     call get_pt_pa
+     mov ecx, 0
+     mov edx, 0
+     call vga_write_hex32
 
-    ; row 1: PD_PA
-    call get_pd_pa
-    mov ecx, 0
-    mov edx, 1
-    call vga_write_hex32
+     ; row 1
+     call get_pd_pa
+     mov ecx, 0
+     mov edx, 1
+     call vga_write_hex32
 
-    call clear_paging_structs
-    call map_identity_kernel_hdr
-    call map_identity_premain
-    call map_kernel_high
- call map_identity_vga
+     call clear_paging_structs
+     call map_identity_kernel_hdr
+     call map_identity_premain
+     call map_kernel_high
+     call map_identity_vga
 
-    ; row 2: PDE[0]
-    call get_pd_pa
-    mov edi, eax
-    mov eax, [edi]
-    mov ecx, 0
-    mov edx, 2
-    call vga_write_hex32
+     ; row 2
+     call get_pd_pa
+     mov edi, eax
+     mov eax, [edi]
+     mov ecx, 0
+     mov edx, 2
+     call vga_write_hex32
 
-    ; row 3: PDE[KERNEL]
-    call get_pd_pa
-    mov edi, eax
-    mov ebx, PDE_INDEX(KERNEL_VA)
-    shl ebx, 2
-    mov eax, [edi + ebx]
-    mov ecx, 0
-    mov edx, 3
-    call vga_write_hex32
+     ; row 3
+     call get_pd_pa
+     mov edi, eax
+     mov ebx, PDE_INDEX(KERNEL_VA)
+     shl ebx, 2
+     mov eax, [edi + ebx]
+     mov ecx, 0
+     mov edx, 3
+     call vga_write_hex32
 
-    ; row 4: PT_PA | flags (value to be written)
-    call get_pt_pa
-    or  eax, PTE_FLAGS
-    mov ecx, 0
-    mov edx, 4
-    call vga_write_hex32
+     ; row 4
+     call get_pt_pa
+     or  eax, PTE_FLAGS
+     mov ecx, 0
+     mov edx, 4
+     call vga_write_hex32
 
-    ; write PDEs
-    call get_pd_pa
-    mov edi, eax
+     call get_pd_pa
+     mov edi, eax
 
-    call get_pt_pa
-    or  eax, PTE_FLAGS
-    mov [edi], eax
+     call get_pt_pa
+     or  eax, PTE_FLAGS
+     mov [edi], eax
 
-    mov ebx, PDE_INDEX(KERNEL_VA)
-    shl ebx, 2
-    mov [edi + ebx], eax
+     mov ebx, PDE_INDEX(KERNEL_VA)
+     shl ebx, 2
+     mov [edi + ebx], eax
 
-    ; row 5: PDE[0] after write
-    mov eax, [edi]
-    mov ecx, 0
-    mov edx, 5
-    call vga_write_hex32
+     ; row 5
+     mov eax, [edi]
+     mov ecx, 0
+     mov edx, 5
+     call vga_write_hex32
 
-    call get_pd_pa
-    mov cr3, eax
+     call get_pd_pa
+     mov cr3, eax
 
-; Debug: print CR0
-mov eax, cr0
-mov ecx, 0
-mov edx, 6
-call vga_write_hex32
+     ; row 6
+     mov eax, cr0
+     mov ecx, 0
+     mov edx, 6
+     call vga_write_hex32
 
-; Debug: print CR3
-mov eax, cr3
-mov ecx, 0
-mov edx, 7
-call vga_write_hex32
+     ; row 7
+     mov eax, cr3
+     mov ecx, 0
+     mov edx, 7
+     call vga_write_hex32
 
-mov word [VGA_VA], 0x1F40
+     ; row 8 — kmain before paging
+     mov eax, kmain
+     mov ecx, 5
+     mov edx, 8
+     call vga_write_hex32
 
-    mov eax, cr0
-    or  eax, (1 << PG_BIT)
-    mov cr0, eax
+     mov word [VGA_VA], 0x1F40
 
-    mov esp, KSTACK_TOP_VA
+     mov eax, cr0
+     or  eax, (1 << PG_BIT)
+     mov cr0, eax
 
-    jmp .pg_on
+     mov esp, KSTACK_TOP_VA
+     jmp .pg_on
 
 .pg_on:
+     mov eax, kmain
+     mov ecx, 0
+     mov edx, 15
+     call vga_write_hex32
 
-
-; Debug: print kmain address
-    mov eax, kmain
-    mov ecx, 0
-    mov edx, 15
-    call vga_write_hex32
-
- mov word [VGA_VA], 0x1F40
-
-    jmp kmain
+     mov word [VGA_VA], 0x1F40
+     jmp kmain
 
 ; --------------------------------------------------
 
 va_to_pa:
-    sub eax, KERNEL_VA
-    add eax, KERNEL_LOAD_PA
-    ret
+     sub eax, KERNEL_VA
+     add eax, KERNEL_LOAD_PA
+     ret
 
 get_pt_pa:
-    mov eax, __kernel_page_table_va
-    jmp va_to_pa
+     mov eax, __kernel_page_table_va
+     jmp va_to_pa
 
 get_pd_pa:
-    mov eax, __kernel_page_directory_va
-    jmp va_to_pa
+     mov eax, __kernel_page_directory_va
+     jmp va_to_pa
 
 clear_paging_structs:
-    xor eax, eax
+     xor eax, eax
 
-    call get_pt_pa
-    mov edi, eax
-    mov ecx, PAGE_SIZE / 4
-    rep stosd
+     call get_pt_pa
+     mov edi, eax
+     mov ecx, PAGE_SIZE / 4
+     rep stosd
 
-    call get_pd_pa
-    mov edi, eax
-    mov ecx, PAGE_SIZE / 4
-    rep stosd
-    ret
+     call get_pd_pa
+     mov edi, eax
+     mov ecx, PAGE_SIZE / 4
+     rep stosd
+     ret
 
 map_page:
-    mov edx, eax
-    shr edx, PAGE_SHIFT
-    and edx, 0x3FF
+     mov edx, eax
+     shr edx, PAGE_SHIFT
+     and edx, 0x3FF
 
-    call get_pt_pa
-    mov edi, eax
+     call get_pt_pa
+     mov edi, eax
 
-    shl edx, 2
-    add edi, edx
+     shl edx, 2
+     add edi, edx
 
-    mov esi, ebx
-    or  esi, ecx
-    mov [edi], esi
-    ret
+     mov esi, ebx
+     or  esi, ecx
+     mov [edi], esi
+     ret
 
 map_identity_kernel_hdr:
-    mov eax, KERNEL_HDR_PA
-    mov ebx, KERNEL_HDR_PA
-    mov ecx, PTE_FLAGS
-    call map_page
-    ret
+     mov eax, KERNEL_HDR_PA
+     mov ebx, KERNEL_HDR_PA
+     mov ecx, PTE_FLAGS
+     call map_page
+     ret
 
 map_identity_premain:
-    mov eax, PREMAIN_PA
-    mov ebx, PREMAIN_PA
-    mov ecx, PTE_FLAGS
-    call map_page
-    ret
+     mov eax, PREMAIN_PA
+     mov ebx, PREMAIN_PA
+     mov ecx, PTE_FLAGS
+     call map_page
+     ret
 
 map_identity_vga:
-    mov eax, VGA_VA
-    mov ebx, VGA_PA
-    mov ecx, PTE_FLAGS
-    call map_page
-    ret
+     mov eax, VGA_VA
+     mov ebx, VGA_PA
+     mov ecx, PTE_FLAGS
+     call map_page
+     ret
 
 map_kernel_high:
-    mov esi, KERNEL_VA
-    mov edi, KERNEL_LOAD_PA
-    mov ecx, PTE_FLAGS
+     mov esi, KERNEL_VA
+     mov edi, KERNEL_LOAD_PA
+     mov ecx, PTE_FLAGS
 
-    mov ebp, KERNEL_MEMORY_SIZE
-    shr ebp, PAGE_SHIFT
+     mov ebp, KERNEL_MEMORY_SIZE
+     shr ebp, PAGE_SHIFT
 
 .loop:
-    mov eax, esi
-    mov ebx, edi
-    push ecx
-    call map_page
-    pop ecx
+     mov eax, esi
+     mov ebx, edi
+     push ecx
+     call map_page
+     pop ecx
 
-    add esi, PAGE_SIZE
-    add edi, PAGE_SIZE
-    dec ebp
-    jnz .loop
-    ret
+     add esi, PAGE_SIZE
+     add edi, PAGE_SIZE
+     dec ebp
+     jnz .loop
+     ret
 
-; eax=value, ecx=x, edx=y
 vga_write_hex32:
-    mov edi, edx
-    imul edi, VGA_COLS
-    add edi, ecx
-    shl edi, 1
-    add edi, VGA_VA
+     mov edi, edx
+     imul edi, VGA_COLS
+     add edi, ecx
+     shl edi, 1
+     add edi, VGA_VA
 
-    mov esi, 8
+     mov esi, 8
 .hex_loop:
-    mov ebx, eax
-    shr ebx, 28
-    and ebx, 0xF
-    cmp bl, 9
-    jbe .digit
-    add bl, ('A' - 10)
-    jmp .emit
+     mov ebx, eax
+     shr ebx, 28
+     and ebx, 0xF
+     cmp bl, 9
+     jbe .digit
+     add bl, ('A' - 10)
+     jmp .emit
 .digit:
-    add bl, '0'
+     add bl, '0'
 .emit:
-    mov byte [edi], bl
-    mov byte [edi + 1], VGA_ATTR
-    add edi, 2
-    shl eax, 4
-    dec esi
-    jnz .hex_loop
-    ret
+     mov byte [edi], bl
+     mov byte [edi + 1], VGA_ATTR
+     add edi, 2
+     shl eax, 4
+     dec esi
+     jnz .hex_loop
+     ret
