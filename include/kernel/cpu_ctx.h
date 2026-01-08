a5c37e74427b608b06a6fee4cb12f6f23b5b2393
Peter Veentjer <alarmnummer@gmail.com>
2026-01-08 06:11:58 +0200

    Minor doc improvement cpu_ctx

--- include/kernel/cpu_ctx.h
+++ include/kernel/cpu_ctx.h
@@ -4,7 +4,11 @@
 struct cpu_ctx
 {
     // we just need the sp/ss, all other cpu context is stored on the stack.
+
+    // userspace stack pointer
     unsigned long u_sp;
+
+    // kernel stack pointer
     unsigned long k_sp;
 };
 
