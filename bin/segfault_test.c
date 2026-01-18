fc5f453d22b0770752924adab1c9b43ed79a85e6
Peter Veentjer <alarmnummer@gmail.com>
2026-01-18 15:24:30 +0200

    segfault_test minor cleanup

--- bin/segfault_test.c
+++ bin/segfault_test.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define BAD_ADDR ((void *)1)
 
+static void show_help(const char *prog)
+{
+    printf("Usage: %s [user|kernel] [--help]\n", prog);
+    printf("  user    trigger a user-space segfault\n");
+    printf("  kernel  trigger a kernel EFAULT via write()\n");
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("Usage: %s [user|kernel]\n", argv[0]);
+    if (argc != 2)
+    {
+        show_help(argv[0]);
         return 1;
     }
 
-    printf("segfault_test\n");
+    if (strcmp(argv[1], "--help") == 0)
+    {
+        show_help(argv[0]);
+        return 0;
+    }
 
-    if (argv[1][0] == 'k') {
+    if (strcmp(argv[1], "kernel") == 0)
+    {
         /* Kernel: should return -EFAULT */
         return write(STDOUT_FILENO, BAD_ADDR, 10);
-    } else {
+    }
+    else if (strcmp(argv[1], "user") == 0)
+    {
         /* User: should segfault */
-        int x = *(volatile int *)BAD_ADDR;
+        int x = *(volatile int *) BAD_ADDR;
         printf("%d\n", x);
         return 0;
+    }
+    else
+    {
+        show_help(argv[0]);
+        return 1;
     }
 }
